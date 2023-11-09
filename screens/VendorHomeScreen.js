553d86aae9be0854c2eef01077a8c6785565bf81
felista47 <manotifelista9@gmail.com>
2023-11-09 06:58:48 +0300

    initialize app and installed dependencies

---
+++ screens/VendorHomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const VendorHomeScreen = () => {
+  return (
+    <View>
+      <Text>VendorHomeScreen</Text>
+    </View>
+  )
+}
+
+export default VendorHomeScreen
