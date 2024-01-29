d0ce3df26b9a6f04f4c5f0af408469d96f889577
felista47 <manotifelista9@gmail.com>
2024-01-29 12:58:33 +0300

    delete duplicates

--- src/reduxStore/actions/CartActions.js
+++
@@ -1,9 +0,0 @@
-// cartActions.js
-
-import { createAction } from "@reduxjs/toolkit";
-
-// Action Creators
-export const addToCart = createAction("cart/addToCart");
-export const removeFromCart = createAction("cart/removeFromCart");
-export const incrementQuantity = createAction("cart/incrementQuantity");
-export const decrementQuantity = createAction("cart/decrementQuantity");
--- src/reduxStore/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartReducer from "./reducers/CartReducer";
-
-
-export const store = configureStore ({
-    reducer:{
-        cart:CartReducer
-    }
-})
