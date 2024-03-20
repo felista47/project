60473459f786cf1aca999207e04b059c0a884169
felista47 <manotifelista9@gmail.com>
2024-03-20 16:42:32 +0300

    perform crud on students

--- client/src/components/Child.js
+++ client/src/components/Child.js
@@ -5,15 +5,17 @@ import { useAuth } from '../context/AuthContext'
 
 
 const Child = () => {
-    const { userEmail} = useAuth();
-    const [children, setChildren] = useState([]);
+    const { uid} = useAuth();
     const [isEditingChild, setIsEditingChild] = useState(false);
     const [editingChild, setEditingChild] = useState(null);
+    const [children, setChildren] = useState([]);
     const [newChild, setNewChild] = useState({
-      parent:'',
-      childFullName: '',
-      gradeClass: '',
-      studentID: '',
+        Name: '',
+        Grade: '',
+        studentID: '',
+        frequency: '',
+        limit: 0,
+        balance: 0
     });
 
     useEffect(() => {
@@ -22,69 +24,67 @@ const Child = () => {
 
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://pocket-money.up.railway.app/children/parent/${userEmail}`);
+        console.log(uid)
+        const response = await axios.get(`http://172.16.120.106:3000/student//${uid}`);
         const parentData = response.data;
         setChildren(parentData);
+        console.error('children data:',parentData);
       } catch (error) {
         console.error('Error fetching parents children data:', error);
       }
     };
 
     const handleInputChange = (name, value) => {
-      setNewChild({ ...newChild, [name]: value }); // Update specific property in newChild object
+      setNewChild({ ...newChild, [name]: value }); 
     };
+    
 
     const handleSubmitNewChild = async () => {
-      setNewChild(prevChild => ({ ...prevChild, parent: userEmail }));
 
-      try {
-        console.log('child', userEmail,newChild);
-        const response = await axios.post(
-          `https://pocket-money.up.railway.app/children`,
-          newChild
-        );
+      try{  
+        const requestData = {
+          data: newChild,
+          uid: uid,
+    
+        };
+          console.log('child',requestData);
+       
+        const response = await axios.post(`http://172.16.120.106:3000/student`,requestData);
     
         if (response.status === 200) {
           console.log('New child added successfully!');
           const parentData = response.data;
           setChildren(parentData);
-          setNewChild({ childFullName: '', gradeClass: '', studentID: '' });
-        } else {
+          setNewChild({
+            Name: '',
+            Grade: '',
+            studentID:'',
+            frequency: '',
+            limit: 0,
+            balance: 0
+        });
+         } else {
           console.error('Error adding child:', response.data);
-        }
+     }
       } catch (error) {
         console.error('Error adding child:', error);
       }
     };
-        
-    const handleDeletePress = async (childId) => {
-      console.log('child id',childId)
-      try {
-        // Call deleteChild with the childId
-        await deleteChild(childId);
-      } catch (error) {
-        console.error('Error handling child press:', error);
-      }
-    }
-    const deleteChild = async (childId) => {
-    try {
-      console.log("before delete requesst",userEmail,childId)
-      const response = await axios.delete(`https://pocket-money.up.railway.app/children/${userEmail}/children/${childId}`);
-        if (response.status === 200) {
-        console.log('Child deleted successfully');
-        await fetchData();
-      } else {
-        console.log('Error deleting child: delete end point', response.data.error);
-      }
-    } catch (error) {
-      console.error('Error deleting child catch err:', error.message);
-    }
-    };
+
     const handleEditChild = (child) => {
       setIsEditingChild(true);
       setEditingChild(child);
     };
-  
+    const handleDeletePress = async () => {
+      try {
+        const response = await axios.delete(`http://172.16.120.106:3000/student/${uid}`);
+        const parentData = response.data;
+        setChildren(parentData);
+        console.error('children data:',parentData);
+      } catch (error) {
+        console.error('Error fetching parents children data:', error);
+      }
+    };
 
  return (
     <ScrollView style={styles.accItem}>
@@ -93,8 +93,8 @@ const Child = () => {
         {children.map((child, index) => (
           <TouchableOpacity key={index}>
             <Text style={styles.accItem}>Child {index + 1}</Text>
-            <Text style={styles.accItem}>Child Full Name: {child.childFullName}</Text>
-            <Text style={styles.accItem}>Grade/Class: {child.gradeClass}</Text>
+            <Text style={styles.accItem}>Child Full Name: {child.Name}</Text>
+            <Text style={styles.accItem}>Grade/Class: {child.Grade}</Text>
             <Text style={styles.accItem}>Student ID: {child.studentID}</Text>
             <View>
             <TouchableOpacity onPress={() => handleDeletePress(child._id)}><Text>Delete</Text></TouchableOpacity>
@@ -106,36 +106,42 @@ const Child = () => {
           </TouchableOpacity>
           ))}
           {isEditingChild && (
-              <>
-              <TextInput
-                editable={true}
-                placeholder="Full Name"
-                // value={editingChild.childFullName}
-                onChangeText={(text) => handleInputChange('childFullName', text)}
-              />
-              <TextInput
-                placeholder="Grade/Class"
-                // value={editingChild.gradeClass}
-                onChangeText={(text) => handleInputChange('gradeClass', text)}
-              />
+            <></>
+              // <>
+              // <TextInput
+              //   editable={true}
+              //   placeholder="Full Name"
+              //   value={editingChild.childFullName}
+              //   onChangeText={(text) => handleInputChange('Name', text)}
+              // />
+              // <TextInput
+              //   placeholder="Grade"
+              //   value={editingChild.gradeClass}
+              //   onChangeText={(text) => handleInputChange('Grade', text)}
+              // />
              
 
-              <TouchableOpacity onPress={() => handleSubmitNewChild(editingChild)}>
-                <Text>Save Changes</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => setIsEditingChild(false)}>
-                <Text>Cancel</Text>
-              </TouchableOpacity>
-              </>
+              // <TouchableOpacity onPress={() => handleSubmitNewChild(editingChild)}>
+              //   <Text>Save Changes</Text>
+              // </TouchableOpacity>
+              // <TouchableOpacity onPress={() => setIsEditingChild(false)}>
+              //   <Text>Cancel</Text>
+              // </TouchableOpacity>
+              // </>
           )}
 
            </>
         ) : (
            <>
            {/* Form for adding a child */}
-             <TextInput placeholder="Full Name" value={newChild.childFullName} onChangeText={(text) => handleInputChange('childFullName', text)}/>
-             <TextInput placeholder="Grade/Class" value={newChild.gradeClass} onChangeText={(text) => handleInputChange('gradeClass', text)}/>
-             <TextInput placeholder="Student ID" value={newChild.studentID} onChangeText={(text) => handleInputChange('studentID', text)}/>
+             <TextInput placeholder="Full Name" value={newChild.Name} onChangeText={(text) => handleInputChange('Name', text)}/>
+             <TextInput placeholder="Grade/Class" value={newChild.Grade} onChangeText={(text) => handleInputChange('Grade', text)}/>
+             <TextInput placeholder="StudentID" value={newChild.studentID} onChangeText={(text) => handleInputChange('studentID', text)}/>
+             <TextInput placeholder="Allowance frequency" value={newChild.frequency} onChangeText={(text) => handleInputChange('frequency', text)}/>
+             <TextInput placeholder="allawonce limit"
+               keyboardType="numeric"
+               value={newChild.limit}
+               onChangeText={(text) => handleInputChange('limit',text )}/>
              <TouchableOpacity onPress={handleSubmitNewChild}><Text>Add Child</Text></TouchableOpacity>
            </>
         )}
